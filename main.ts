c1daca171b5b2d70c2dee637ddbb67edcfd64ad6
Jack149 <31501037+Jack149@users.noreply.github.com>
2021-12-19 20:44:28 -0600

    Added TILT and changed sounds a little. Still appears to work

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 111) {
+        TILT += 1
         music.setBuiltInSpeakerEnabled(true)
         music.playTone(262, music.beat(BeatFraction.Whole))
         music.stopAllSounds()
@@ -9,14 +10,13 @@ radio.onReceivedNumber(function (receivedNumber) {
 })
 input.onButtonPressed(Button.A, function () {
     if (true) {
-        basic.showNumber(0)
+        basic.showNumber(TILT)
         basic.pause(2000)
         basic.clearScreen()
     }
 })
+let TILT = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
 music.setVolume(255)
-basic.forever(function () {
-	
-})
+TILT = 0
