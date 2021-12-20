96671a78e2cce453f7c54402168e63363c1b5660
Jack149 <31501037+Jack149@users.noreply.github.com>
2021-12-19 18:00:36 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,12 +2,21 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 111) {
         music.setBuiltInSpeakerEnabled(true)
         music.playTone(262, music.beat(BeatFraction.Whole))
+        music.stopAllSounds()
     } else {
         music.stopAllSounds()
     }
 })
+input.onButtonPressed(Button.A, function () {
+    if (true) {
+        basic.showNumber(0)
+        basic.pause(2000)
+        basic.clearScreen()
+    }
+})
 radio.setGroup(1)
 radio.setTransmitPower(7)
+music.setVolume(255)
 basic.forever(function () {
 	
 })
